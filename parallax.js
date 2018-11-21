8c1ef43767d606ceb85855146f41fe7db2d2bc9c
mePixel <xdxaverdrabik@gmail.com>
2018-11-21 18:24:40 +0100

    Add files via upload

--- parallax.js
+++ parallax.js
@@ -7,16 +7,17 @@ function parallax() {
   var wScroll = $(window).scrollTop()
 
 
-  $('.parallax--bg1').css('background-position', 'center ' + (wScroll * 0.04) + 'px');
+  $('.parallax--bg1').css('background-position', 'center ' + (wScroll * 0.4) + 'px');
 
-  $('.parallax--bg2').css('background-position', 'center ' + (wScroll * 0.03) + 'px');
+  $('.parallax--bg2').css('background-position', 'center ' + (wScroll * 0.3) + 'px');
 
-  $('.parallax--bg3').css('background-position', 'center ' + (wScroll * 0.02) + 'px');
+  $('.parallax--bg3').css('background-position', 'center ' + (wScroll * 0.2) + 'px');
 
-  $('.parallax--bg4').css('background-position', 'center ' + (wScroll * 0.01) + 'px');
+  $('.parallax--bg4').css('background-position', 'center ' + (wScroll * 0.1) + 'px');
 
   console.log(wScroll);
 
+  $('html').css('scroll-behavior', 'smooth');
 
   $('.parallax--box').css('top', -5 + (wScroll * .005) + 'em')
 
